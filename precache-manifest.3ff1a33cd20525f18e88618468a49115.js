af9198ff2c26bcb569bb68c3215cd50f57c61a6e
Lisa Smithen <smithenlisa@gmail.com>
2019-12-11 19:46:56 -0600

    Updates

---
+++ precache-manifest.3ff1a33cd20525f18e88618468a49115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a966204de49d92078a6b2ef0e1c5d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25367136dc1c2d8aae69",
+    "url": "/static/css/main.fd3dd5ed.chunk.css"
+  },
+  {
+    "revision": "9faf0b82856067b0f86c",
+    "url": "/static/js/2.307ae932.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.307ae932.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25367136dc1c2d8aae69",
+    "url": "/static/js/main.cf0d570f.chunk.js"
+  },
+  {
+    "revision": "64db1e7c90e834bd164e",
+    "url": "/static/js/runtime-main.43ae31c7.js"
+  }
+]);
